9370c7b4d5e83bfe9370f96bebf2ad3d0215efec
Ethan <ethanghoul@gmail.com>
2022-05-12 19:55:21 -0700

    Fixed incorrect register use.

--- FinalProject.asm
+++ FinalProject.asm
@@ -1,3 +1,5 @@
+; 
+
 .orig x3000
 
         AND R5, R5, #0
@@ -6,8 +8,9 @@ LOOP    JSR INPUT
         ADD R5, R5, #-1
         BRp LOOP
         HALT
-
-INPUT   AND R0, R0, #0
+INPUT   ST R7, SAVEREG7
+	AND R0, R0, #0
+        AND R1, R1, #0
         AND R4, R4, #0
         AND R2, R2, #0
         AND R3, R3, #0
@@ -33,13 +36,14 @@ INPUT   AND R0, R0, #0
         ; ADD R4, R3, #-9         ;R4 = OFFSET 9 from R3, THIS IS DONE FOR INPUT VALIDATION 0-9 for 1s place
         ; BRp ERROR               ;GO TO ERROR IF INPUT IS LARGER THAN 9
         AND R0, R0, #0
-        AND R4, R4, x0          ;Clearing R4 to use Again
         LEA R0, LF
         PUTS
         
         LD R4, TEN              ;used to multiply by 10
-        JSR BY10 
-        ADD R4, R1, R2
+        JSR BY10
+        AND R4, R4, #0          ;Clearing R4 to use Again
+        ADD R4, R1, R3
+        LD R7, SAVEREG7
         RET
         
 BY10    ADD R1, R1, R2          ;Multiplying by 10
@@ -48,8 +52,24 @@ BY10    ADD R1, R1, R2          ;Multiplying by 10
         ADD R1, R1, x0
         RET
 
+; push subroutine (R4 = input, R6 = stack location)
+PUSH	STR R4, R6, X0	; store R4 into address held in R6
+	ADD R6, R6, x-1	; move up stack
+
+; pop subroutine (R4 = output, R6 = stack location)
+POP	ADD R6, R6, X1	; move down stack
+	LDR R4, R6, X0	; load R4 with value at address held in R6
+
+; data -----------------------------------------------
+; (i) = input, (c) = caluclated during execution, no () = constant
+
 TEN .FILL x000A
 
 PROMPT .STRINGZ "ENTER TEST SCORE: "
 LF      .STRINGZ "\n"
+
+BASE	.FILL X4000	; base of test score stack
+
+SAVEREG7        .FILL X3200     ; (c) saves register 7 (for subroutines within subroutines)
+
 .END
